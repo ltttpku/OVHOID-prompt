62475100dd10a32b1aa0ebad3d723b6d33e2e7e6
ltttpku <ting_lei@pku.edu.cn>
2023-10-30 01:35:22 +0800

    slurm

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ def main(args):
         if args.output_dir:
             checkpoint_paths = [output_dir / 'checkpoint.pth']
             # extra checkpoint before LR drop and every 100 epochs
-            if (epoch + 1) % args.lr_drop == 0 or epoch % 10 == 0:
+            if (epoch + 1) % args.lr_drop == 0 or (epoch % 10 == 0 and epoch >= 80):
                 checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
             for checkpoint_path in checkpoint_paths:
                 utils.save_on_master({
