406d4f619237669231b75a4bb0129e288f8dc551
ltttpku <ting_lei@pku.edu.cn>
2023-11-11 02:30:03 +0800

    slurm

--- arguments.py
+++ arguments.py
@@ -126,7 +126,7 @@ def get_args_parser():
     parser.add_argument('--eval_size', default=448, type=int, help="image resolution for evaluation")
     parser.add_argument('--vis_outputs', action='store_true', help='visualize the model outputs')
     parser.add_argument('--vis_dir', default='', help='path where to save visualization results')
-    parser.add_argument('--bbox_lambda', default=1.0, type=float)
+    parser.add_argument('--bbox_lambda', default=2.0, type=float)
     parser.add_argument('--aux_text_weight', default=1.0, type=float)
     parser.add_argument('--enable_softmax', action='store_true')
     parser.add_argument('--best_beta', default=1.0, type=float)
