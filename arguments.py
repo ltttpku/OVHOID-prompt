fabb968b4c85158211ffa481e0710908187440fe
ltttpku <ting_lei@pku.edu.cn>
2023-11-13 02:50:52 +0800

    slurm

--- arguments.py
+++ arguments.py
@@ -76,7 +76,7 @@ def get_args_parser():
     parser.add_argument('--no_aux_loss', dest='aux_loss', action='store_false',
                         help="Disables auxiliary decoding losses (loss at each layer)")
     parser.add_argument('--enable_focal_loss', action='store_true', help='enable decoders')
-    parser.add_argument('--focal_alpha', default=0.5, type=float)
+    parser.add_argument('--focal_alpha', default=   , type=float)
     parser.add_argument('--focal_gamma', default=0.2, type=float)
     # * Matcher
     parser.add_argument('--set_cost_class', default=5, type=float,
